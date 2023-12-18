bd0405b7aedf5a515cb32202a5be20657d1a82da
Bajuddin Khan <bajuddinkhan786@gmail.com>
2023-12-18 21:04:42 +0530

    day 5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learn/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_learn/pages/home_page.dart';
 import 'package:flutter_learn/pages/login_page.dart';
@@ -18,12 +19,13 @@ class MyApp extends StatelessWidget {
             appBarTheme: AppBarTheme(backgroundColor: Colors.pink),
             fontFamily: GoogleFonts.lato().fontFamily,
             primaryTextTheme: GoogleFonts.latoTextTheme()),
+        debugShowCheckedModeBanner: false,
         darkTheme: ThemeData(brightness: Brightness.dark),
         initialRoute: "/",
         routes: {
           "/": (context) => LoginPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage()
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage()
         });
   }
 }
